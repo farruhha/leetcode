398a78e074c153d80a71017165d1d7733db84bc7
Farruh Habibullaev <farruh.habibullaev@gmail.com>
2021-01-27 21:26:50 -0800

    Add Digits

---
+++ src/math/easy/AddDigits.java
@@ -0,0 +1,20 @@
+package math.easy;
+
+public class AddDigits {
+    public int addDigits(int num) {
+
+        while(num >= 10){
+            num = getDigitSums(num);
+        }
+        return num;
+    }
+
+    private int getDigitSums(int num){
+        int sum = 0;
+        while(num > 0){
+            sum += num % 10;
+            num = num / 10;
+        }
+        return sum;
+    }
+}
