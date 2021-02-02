0abccc00b8684c8809a364d5e65483d2a5385b70
Farruh Habibullaev <farruh.habibullaev@gmail.com>
2021-02-02 10:14:11 -0800

    Perfect number

--- src/math/easy/PerfectNumber.java
+++ src/math/easy/PerfectNumber.java
@@ -1,7 +1,19 @@
 package math.easy;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class PerfectNumber {
     public boolean checkPerfectNumber(int num) {
+        return optimal(num);
+    }
+
+    private boolean optimal(int num){
+        Set<Integer> perfectNumbers = Set.of(6, 28, 496, 8128, 33550336);
+        return perfectNumbers.contains(num);
+    }
+
+    private boolean bruteForce(int num){
         int sum = 1;
         for (int number = 2; number <= num / 2; number++){
             if (num  % number == 0){
