3d6280b6c3fec3354984e70286fd2496d589fb95
Farruh Habibullaev <farruh.habibullaev@gmail.com>
2021-02-03 20:56:27 -0800

    Power of Two

--- src/math/easy/CheckIfStraightLine.java
+++ src/math/easy/CheckIfStraightLine.java
@@ -5,13 +5,25 @@ public class CheckIfStraightLine {
     public boolean checkStraightLine(int[][] coordinates) {
         if(coordinates.length == 2) return true;
 
+        /**
+         * According to this𝑦2−𝑦1/𝑥2−𝑥1=𝑦3−𝑦1/𝑥3−𝑥1
+         * */
+
         for(int index = 1; index < coordinates.length - 1; index++){
-            if(!(Math.abs(coordinates[index - 1][0] - coordinates[index][0])
-                    == Math.abs(coordinates[index][0] - coordinates[index + 1][0])
-                    && Math.abs(coordinates[index - 1][1] - coordinates[index][1])
-                    == (Math.abs(coordinates[index][1] - coordinates[index + 1][1])))){
-                return false;
+            int x1 = coordinates[index - 1][0];
+            int y1 = coordinates[index - 1][1];
+
+            int x2 = coordinates[index][0];
+            int y2 = coordinates[index][1];
+
+            int x3 = coordinates[index + 1][0];
+            int y3 = coordinates[index + 1][1];
+
+
+            if ((x1- x2 == 0 || x1 - x3 == 0)){
+               if(x1 - x2 != 0 || x1 - x3 != 0 || y1 - y2 != 0 || y1 - y3 != 0) return false;
             }
+            if ((y2-y1)/(x2-x1) != (y3 - y1)/ (x3 - x1)) return false;
         }
         return true;
     }
