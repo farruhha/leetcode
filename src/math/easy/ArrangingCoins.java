cac9d5e54eb0c2f45319ec526c50fe47dbd86b29
Farruh Habibullaev <farruh.habibullaev@gmail.com>
2021-02-10 23:23:32 -0800

    I have attached implementation

--- src/math/easy/ArrangingCoins.java
+++ src/math/easy/ArrangingCoins.java
@@ -1,9 +1,5 @@
 package math.easy;
 
-import java.math.BigInteger;
-
-import static java.lang.Math.sqrt;
-
 public class ArrangingCoins {
     public static void main(String[] args) {
         System.out.println(new ArrangingCoins().arrangeCoins(1804289383));
@@ -11,7 +7,8 @@ public class ArrangingCoins {
     public int arrangeCoins(int n) {
         return optimalSolution(n);
     }
-
+    /***
+     * https://leetcode.com/problems/arranging-coins/discuss/1060387/One-line-constant-space-and-time-brute-force-solution-in-Java. */
     private int optimalSolution(int n){
         return (int)(-1+ Math.sqrt(1+8* (long) n))/2;
     }
