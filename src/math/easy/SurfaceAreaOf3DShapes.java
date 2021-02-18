aa356a0d17756e7bba02e4a994408a5b85e0b2ec
Farruh Habibullaev <farruh.habibullaev@gmail.com>
2021-02-17 23:43:22 -0800

    Format

--- src/math/easy/SurfaceAreaOf3DShapes.java
+++ src/math/easy/SurfaceAreaOf3DShapes.java
@@ -1,11 +1,14 @@
 package math.easy;
 
+import java.util.Map;
+
 public class SurfaceAreaOf3DShapes {
     public int surfaceArea(int[][] grid) {
         int area = 0;
         for(int row = 0; row < grid.length; row++){
             for(int col = 0; col < grid[row].length; col++){
-                area += getValue(row, col, grid);
+                System.out.println(getValue(row, col, grid) + grid[row][col]);
+                area += getValue(row, col, grid) ;
             }
         }
         return area;
@@ -13,10 +16,10 @@ public class SurfaceAreaOf3DShapes {
 
     private int getValue(int row, int col, int[][] grid){
         int sum = 0;
-        sum += (row - 1 < 0) ? grid[row][col] : ((grid[row - 1][col] < grid[row][col]) ? Math.abs(grid[row - 1][col] - grid[row][col]) : 0);
-        sum += (row + 1 >= grid.length) ? grid[row][col] : ((grid[row + 1][col] < grid[row][col]) ? Math.abs(grid[row + 1][col] - grid[row][col]) : 0);
-        sum += (col - 1 < 0) ? grid[row][col] : ((grid[row][col - 1] < grid[row][col]) ? Math.abs(grid[row][col - 1] - grid[row][col]) : 0);
-        sum += (col + 1 >= grid[row].length) ? grid[row][col] : ((grid[row][col + 1] < grid[row][col]) ? Math.abs(grid[row][col + 1] - grid[row][col]) : 0);
-        return sum + grid[row][col];
+        sum += (row - 1 < 0) ? grid[row][col] : Math.max(grid[row][col] - grid[row - 1][col],0);
+        sum += (row + 1 >= grid.length) ? grid[row][col] : Math.max(grid[row][col] - grid[row + 1][col],0);
+        sum += (col - 1 < 0) ? grid[row][col] : Math.max(grid[row][col] - grid[row][col - 1], 0);
+        sum += (col + 1 >= grid[row].length) ? grid[row][col] : Math.min(grid[row][col] - grid[row][col + 1], 0);
+        return sum + + grid[row][col];
     }
 }
---
+++ src/math/medium/UglyNumber2.java
@@ -0,0 +1,10 @@
+package math.medium;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class UglyNumber2 {
+    public int nthUglyNumber(int n) {
+        return 0;
+    }
+}
